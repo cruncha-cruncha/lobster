98fbd164b35a00195bd5134416a5ffe05288d279
Liam Marcassa <liam@gamesheetinc.com>
2023-10-23 21:49:47 -0400

    Add prettier

--- lobster/src/App.jsx
+++ lobster/src/App.jsx
@@ -1,18 +1,16 @@
-import { Post, fakePost } from './pages/post'
-import { Comments, fakePostData, fakeComments } from './pages/Comments'
-import { Profile, fakeProfile } from './pages/Profile'
-import './App.css'
+import { Post, fakePost } from "./pages/post";
+import { Comments, fakePostData, fakeComments } from "./pages/Comments";
+import { Profile, fakeProfile } from "./pages/Profile";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <div id="app">
       {/* <Post post={fakePost} /> */}
       <Comments postData={fakePostData} comments={fakeComments} />
       {/* <Profile profile={fakeProfile} /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
