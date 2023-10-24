ab968ef9f4f0c32579f1d0b12aca01e0bcd26880
Liam Marcassa <liam@gamesheetinc.com>
2023-10-23 21:49:31 -0400

    Add rust warp hello world

---
+++ lobster-tail/src/main.rs
@@ -0,0 +1,15 @@
+use warp::Filter;
+
+// > cargo run
+// http://localhost:3030/hello/WHATEVER
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
