606493b7b8288abcb5e7bf3180d8f296d1034cba
Liam Marcassa <liam@gamesheetinc.com>
2023-10-26 20:37:55 -0400

    Rust warp out, actix in

--- lobster-tail/src/main.rs
+++ lobster-tail/src/main.rs
@@ -1,15 +1,37 @@
-use warp::Filter;
+//use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use sqlx::postgres::PgPoolOptions;
 
-// > cargo run
-// http://localhost:3030/hello/WHATEVER
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
 
-#[tokio::main]
-async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+// #[get("/users/{user_id}/{friend}")] // <- define path parameters
+// async fn echo(path: web::Path<(u32, String)>) -> Result<String> {
+//     let (user_id, friend) = path.into_inner();
+//     Ok(format!("Welcome {}, user_id {}!", friend, user_id))
+// }
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+#[actix_web::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://wilder:TheFaultInOur”DDL”@localhost/wilder").await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool).await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
+
+    // HttpServer::new(|| {
+    //     App::new()
+    //         .service(hello)
+    //         .service(echo)
+    // })
+    // .bind(("127.0.0.1", 8080))?
+    // .run()
+    // .await
 }
