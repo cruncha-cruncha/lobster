b5743f0e375efa1a252535c0efef4ec9260520a0
Liam Marcassa <markassa@gmail.com>
2024-12-07 22:03:26 -0500

    Remove redundant sql

--- sql/default_data/fixed.sql
+++ sql/default_data/fixed.sql
@@ -1,48 +1,3 @@
-CREATE SCHEMA fixed;
-
-CREATE TABLE fixed.roles (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (name)
-);
-
-CREATE TABLE fixed.user_statuses (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (name)
-);
-
-CREATE TABLE fixed.permission_statuses (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (name)
-);
-
-CREATE TABLE fixed.store_statuses (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (name)
-);
-
-CREATE TABLE fixed.tool_statuses (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (name)
-);
-
-CREATE TABLE fixed.grievance_statuses (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (name)
-);
-
-
 INSERT INTO fixed.roles (name)
 VALUES ('library_admin'), ('user_admin'), ('store_admin'), ('store_rep'), ('tool_manager');
 
@@ -59,5 +14,5 @@ INSERT INTO fixed.tool_statuses (name)
 VALUES ('available'), ('rented'), ('maintenance'), ('broken'), ('lost'), ('stolen'), ('retired');
 
 INSERT INTO fixed.grievance_statuses (name)
-VALUES ('open'), ('closed'), ('resolved'), ('escalated'), ('dismissed'), ('ban'), ('suspension');
+VALUES ('open'), ('closed'), ('resolved'), ('dismissed'), ('ban'), ('suspension');
 
