a5a55878ae0a72b93e3209cbdf392cab54226e2f
Liam Marcassa <markassa@gmail.com>
2025-01-13 05:19:13 -0500

    Frontend: classy main menu

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -15,46 +15,45 @@ export const Layout = () => {
   };
 
   return (
-    <div
-      className="flex min-h-full items-stretch justify-center"
-      onClick={closeMenuIfOpen}
-    >
-      <div className="relative w-full max-w-5xl p-2">
-        <div className="text-center">
+    <div className="min-h-full" onClick={closeMenuIfOpen}>
+      <div className="relative w-full p-2">
+        <div
+          className="relative cursor-pointer text-center"
+          onClick={() => setShowMenu((prev) => !prev)}
+        >
+          <p>{libraryInfo.name}</p>
           <div
-            className="relative cursor-pointer"
-            onClick={() => setShowMenu((prev) => !prev)}
+            className={
+              "z-30 w-full bg-white" + (showMenu ? " absolute" : " hidden")
+            }
           >
-            <p>{libraryInfo.name}</p>
-            <div
-              className={
-                "w-full bg-white" + (showMenu ? " absolute" : " hidden")
-              }
-            >
-              <div
-                className={
-                  "mt-1 w-full border-y border-black pb-2 text-left transition-colors *:my-1 *:block *:px-2 *:duration-150 hover:*:bg-blue-900 hover:*:text-white"
-                }
-              >
-                <NavLink to="/people" className="!mt-2 hover:bg-blue-900">
-                  People
-                </NavLink>
-                <NavLink to="/grievances">Grievances</NavLink>
-                <NavLink to="/rentals">Rentals</NavLink>
-                <NavLink to="/tools">Tools</NavLink>
-                <NavLink to="/stores">Stores</NavLink>
-                <NavLink to="/library">Library</NavLink>
-                <button
-                  onClick={() => auth.logout()}
-                  className="block w-full text-left hover:!bg-transparent hover:!text-black"
-                >
-                  logout
-                </button>
+            <div className="mt-2 w-full border-y border-black pb-2 text-left transition-colors">
+              <div className="flex items-stretch justify-center">
+                <div className="relative w-full max-w-5xl *:my-1 *:block *:px-2 *:duration-150 hover:*:bg-blue-900 hover:*:text-white">
+                  <NavLink to="/people" className="!mt-2 hover:bg-blue-900">
+                    People
+                  </NavLink>
+                  <NavLink to="/grievances">Grievances</NavLink>
+                  <NavLink to="/rentals">Rentals</NavLink>
+                  <NavLink to="/tools">Tools</NavLink>
+                  <NavLink to="/stores">Stores</NavLink>
+                  <NavLink to="/library">Library</NavLink>
+                  <button
+                    onClick={() => auth.logout()}
+                    className="block w-full text-left hover:!bg-transparent hover:!text-black"
+                  >
+                    - logout
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <Outlet />
+      </div>
+      <div className="flex items-stretch justify-center">
+        <div className="relative w-full max-w-5xl p-2">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
